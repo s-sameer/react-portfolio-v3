0bbcf3a85d7967b38232c666382776b3395f160d
Sadman Sameer <itsnotsameer@gmail.com>
2024-05-15 15:18:13 -0230

    experience section done

--- data/index.ts
+++ data/index.ts
@@ -118,65 +118,36 @@ export const navItems = [
     }
   ]
   
-  export const companies = [
-    {
-      id: 1,
-      name: "cloudinary",
-      img: "/cloud.svg",
-      nameImg: "/cloudName.svg",
-    },
-    {
-      id: 2,
-      name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
-    },
-    {
-      id: 3,
-      name: "HOSTINGER",
-      img: "/host.svg",
-      nameImg: "/hostName.svg",
-    },
-    {
-      id: 4,
-      name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
-    },
-    {
-      id: 5,
-      name: "docker.",
-      img: "/dock.svg",
-      nameImg: "/dockerName.svg",
-    },
-  ];
-  
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "AI/Software Engineering Co-op",
+      company: "Mission Control Space Services",
+      desc: "Contributed to the development of a deep learning model for real-time fire event detection using Python and Pytorch",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Full Stack Developer (Co-op)",
+      company: "SiftMed",
+      desc: "Implemented new features, developed robust APIs, and optimized application performance using React, Python, and AWS",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Software Engineering Co-op",
+      company: "Memorial University",
+      desc: "Developed a data visualization application to extract insights from satellite data using React, Tailwind, and Rust",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Software Developer Co-op",
+      company: "C-CORE",
+      desc: "Created several responsive landing pages for the company’s product line, using HTML, CSS, and JavaScript",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
