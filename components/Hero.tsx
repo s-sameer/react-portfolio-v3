89094ab569cbf0109f27ee08b8e5790613e9edde
Sadman Sameer <itsnotsameer@gmail.com>
2024-12-25 17:42:08 -0330

    update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col justify-center items-center'>
                 <TextGenerateEffect 
                     className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                    words='Transforming Your Ideas From Concept To Production'
+                    words='Transforming Your Ideas From Concept To Reality'
                 />
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                     Hi, I&apos;m Sameer, a passionate Full-Stack Developer and Cloud Engineer.
